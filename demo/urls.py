ee92a85f748b95c68b0994031d86a91634d59f8a
shrazo <saddam.hossain.raju.024@gmail.com>
2020-06-04 17:08:27 +0600

    First commit

---
+++ demo/urls.py
@@ -0,0 +1,8 @@
+from django.contrib import admin
+from django.urls import path, include 
+from temp import views
+
+urlpatterns = [
+    path('admin/', admin.site.urls),
+    path('', include('temp.urls')),
+]
